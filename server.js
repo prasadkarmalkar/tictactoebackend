28151a46e74723c8fb849923a1f0cc3fc0964ecd
Prasad Karmalkar <prasadkarmalkar2020@gmail.com>
2020-12-17 21:25:27 +0530

    6th commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var io = require('socket.io')(server,{
 var cors = require('cors');
 
 app.use(cors())
-io.origins('*:*')
+
 const users = [];
 const port = process.env.PORT || 5000
 io.on('connection', (socket) => {
